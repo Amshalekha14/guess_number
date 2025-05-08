25e91a01acead0e38956f1d6252e84b417babe78
Amshalekha14 <amshalekhaaravii@gmail.com>
2025-05-08 14:48:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//selecting Elements
+var inputBox = document.getElementById("inputBox")
+var result = document.getElementById("result")
+var guesscount = document.getElementById("guesscount")
+var noofguess = 3
+//generate random number from 1 to 5
+var randomnumber = Math.floor(Math.random()*10)+1 //1 to 10
+if(randomnumber>5)
+{
+    randomnumber=randomnumber-5
+}
+//Event handler
+//  function
+function checkthenumber()
+{
+    if(inputBox.value == randomnumber)
+    {
+        alert("you got it right,congratulations")
+        result.textContent = "you are right !"
+    }
+    else{
+        noofguess=no=noofguess-1
+        if(noofguess==0)
+        {
+            alert("you lost,generated Random number :"+randomnumber)
+        }        
+        alert("you lost, generated random number is :"+ randomnumber)
+    }
+
+    guesscount.textcontent = "Available guesses : " +noofguess
+    result.textcontent = "You are wrong !"
+
+
+    }
